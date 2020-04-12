fb4be3ac53023ab60b317d457fe58a92b1909165
engrzakaug1989 <59294845+engrzakaug1989@users.noreply.github.com>
2020-04-12 18:11:13 +0500

    Add files via upload

---
+++ Generator-printing-perfect-squares.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 12 17:54:20 2020
+
+@author: Hp
+Usage of generators to free up memory. Print perfect squares of given range of numbers
+"""
+
+
+
+def perfect_square(num):
+    ''' We can also use while loop here while i <= num: yield i**2 '''
+    for i in range(1, num + 1):
+        yield i **2
+    
+
+if __name__ == '__main__':
+    
+    num = int(input("Enter range: "))
+    gen = perfect_square(num)
+    
+    type(gen)
+    
+    for var in gen:
+        print(var)
