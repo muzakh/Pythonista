4eddce768642aa966424e58192d1ab28c3eb9851
engrzakaug1989 <59294845+engrzakaug1989@users.noreply.github.com>
2020-04-12 21:13:10 +0500

    Add files via upload

---
+++ GeneratorExpressions-Vs-ListComprehensions.py
@@ -0,0 +1,36 @@
+"""
+Created on Sun Apr 12 20:44:55 2020
+
+@author: Zohaib
+Decription: List comprehension Vs Generator Expressions
+
+List Comprehension : Generates a list using for loop. Written in square brackets and returns a complete list. Memory hungry.
+
+Generator Expression : Returns element of a list one by one. Syntax is same as that of list comprehensions but its written in small brackets.Memory efficient
+"""
+
+interator = 11
+
+""" List Comprehension """
+# Calsulates square of nunbers within given range of numbers greater than 5
+alist = [i**2 for i in range(interator) if i > 5]
+print(alist)
+print("Data type is a : " + str(type(alist)))
+
+""" Generator Expressions """
+# Calsulates square of nunbers within given range of numbers greater than 5
+
+def gen_exp_fn():
+    return (i**2 for i in range(interator) if i > 5)
+
+gen_exp = gen_exp_fn()
+print("\nGenerator Function has {0}" . format(gen_exp))
+
+print(type(gen_exp))
+
+''' Now printing the values of a generator or developing a list from the generator func using a for loop'''
+print("\n\n *** Applying Generator Function again to refresh generator object and fetching elements one by one using the loop ***\n")
+gen_exp = gen_exp_fn()
+
+for var in gen_exp:
+    print(var)
